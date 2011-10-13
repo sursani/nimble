9ec5b3aa91b606e36169ca113ea0c024118ca1a5
Shane Ursani <shaneursani@gmail.com>
2011-10-13 10:27:50 -0400

    add url parsing for tweets with livequery. move js from page to nimble.js

--- public/javascripts/nimble.js
+++ public/javascripts/nimble.js
@@ -11,11 +11,11 @@ var showOldTweets = function (tweets) {
 };
 
 var formatTweetList = function (data) {
-	return '<li class="tweet-stream"><div class="profile-image"><img src="' + data.profile_image_url + '"/></div><div class="tweet-info"><div class="user">' + data.full_name + ' (' + data.user_name + ')' + '</div><div class="text">' + data.text + '</div><div class="date"><abbr class="timeago" title="' + ISODateString(new Date(data.created_on)) + '"></abbr></div></div></li>';
+	return '<li class="tweet-stream"><div class="profile-image"><img src="' + data.profile_image_url + '"/></div><div class="tweet-info"><div class="user">' + data.full_name + ' (@' + data.user_name + ')' + '</div><div class="text">' + data.text + '</div><div class="date"><abbr class="timeago" title="' + ISODateString(new Date(data.created_on)) + '"></abbr></div></div></li>';
 };
 
 var ISODateString = function (d) {
-    function pad(n) {
+    function pad (n) {
         return n < 10 ? '0' + n : n
     }
     return d.getUTCFullYear()+'-'
@@ -26,6 +26,39 @@ var ISODateString = function (d) {
     	+ pad(d.getUTCSeconds())+'Z';
 };
 
+var replaceURLWithHTMLLinks = function (text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp, '<a href="$1" target="_blank">$1</a>');
+};
+
+$('#showMore').click(function(e) {
+	e.preventDefault();
+	$.post('/getmoretweets', { page: nimbleGlobal.nextPage }, function (data) {
+		if (data && data.length > 0) {
+			nimbleGlobal.nextPage = nimbleGlobal.nextPage + 1;
+			showOldTweets(data);
+		}
+	});
+});
+
+// DOM ready
+$(function () {
+	$('.date').each(function(i) {
+		var the_date = new Date($(this).text());
+		$(this).html('<abbr class="timeago" title="' + ISODateString(the_date) + '"></abbr>');
+	});
+	
+	$('abbr.timeago').livequery(function () {
+		$(this).timeago();
+	});
+	
+	$('.text').livequery(function () {
+		var text = replaceURLWithHTMLLinks($(this).text());
+		$(this).html(text);
+	});
+});
+
+// Socket IO
 var socket = io.connect();
 
 socket.on('connect', function() {
