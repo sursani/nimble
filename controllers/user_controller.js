13560374d4cb9f986ef7ea08f45873e8b1c750d3
Shane Ursani <shaneursani@gmail.com>
2011-10-13 16:14:51 -0400

    new style for site

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,25 +3,32 @@ var twitter = require('twitter');
 
 var TweetProvider = require('../providers/tweet_provider').TweetProvider;
 var TweetProvider = new TweetProvider();
-//var FriendProvider = require('../providers/friend_provider').FriendProvider;
-//var friendProvider = new FriendProvider();
+var FriendProvider = require('../providers/friend_provider').FriendProvider;
+var friendProvider = new FriendProvider();
 
 module.exports = {
 	displayUserTweets: function (req, res) {
 		TweetProvider.getPagedTweetsByUser(1, req.params.user_name, function(err, docs) {
 			if (!err && docs.length > 0) {
-				var viewModel = {
-					user_name: req.params.user_name,
-					tweets: docs
-				};
+				friendProvider.find(function(err, friends) {
+					if (!err) {
+						var viewModel = {
+							user_name: req.params.user_name,
+							tweets: docs,
+							friends: friends
+						};
 				
-				var userInfo = {};
-				userInfo.user_name = docs[0].user_name;
-				userInfo.full_name = docs[0].full_name;
+						var userInfo = {};
+						userInfo.user_name = docs[0].user_name;
+						userInfo.full_name = docs[0].full_name;
 				
-				res.render('index/user', {
-					title: userInfo.full_name + ' (@' + userInfo.user_name + ') - Nimble Celebrity Tweets',
-					model: viewModel
+						res.render('user/user', {
+							title: userInfo.full_name + ' (@' + userInfo.user_name + ') - Nimble Celebrity Tweets',
+							model: viewModel
+						});
+					} else {
+						res.end();
+					}
 				});
 			} else {
 				res.end();
