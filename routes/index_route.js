8fb814bd56620b3861717cd58cdccdf95cd2d7cb
Shane Ursani <shaneursani@gmail.com>
2011-10-22 21:38:28 -0400

    uncomment about route

--- routes/index_route.js
+++ routes/index_route.js
@@ -2,5 +2,5 @@ var indexController = require('../controllers/index_controller');
 
 module.exports = function(app) {	
 	app.get('/', indexController.index);
-	//app.get('/about', indexController.about);
+	app.get('/about', indexController.about);
 };
