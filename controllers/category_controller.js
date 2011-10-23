a06456d403fbf58c4024d4900fe505020b7856c3
Shane Ursani <shaneursani@gmail.com>
2011-10-22 22:26:00 -0400

    fix some small bugs

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -8,49 +8,49 @@ module.exports = {
 
 	index: function (req, res, next) {
 		var category = req.params.category.toLowerCase();
-		if (!nimble.isValidCategory(category)) {
+		if (nimble.isValidCategory(category)) {
+			var tweetProvider = new TweetProvider(category);
+			var friendProvider = new FriendProvider(category);
+			tweetProvider.getPagedTweets(1, function(err, docs) {
+				if (!err) {
+					friendProvider.find(function(err, friends) {
+						if (!err) {
+							var viewModel = {
+								tweets: docs,
+								friends: friends,
+								category: category
+							};
+						
+							res.render('category/index', {
+								title: 'Nimble Celebrity Tweets',
+								model: viewModel
+							});
+						} else {
+							next();
+						}
+					});
+				} else {
+					next();
+				}
+			});
+		} else {
 			next();
 		}
-		
-		var tweetProvider = new TweetProvider(category);
-		var friendProvider = new FriendProvider(category);
-		tweetProvider.getPagedTweets(1, function(err, docs) {
-			if (!err) {
-				friendProvider.find(function(err, friends) {
-					if (!err) {
-						var viewModel = {
-							tweets: docs,
-							friends: friends,
-							category: category
-						};
-						
-						res.render('category/index', {
-							title: 'Nimble Celebrity Tweets',
-							model: viewModel
-						});
-					} else {
-						next();
-					}
-				});
-			} else {
-				next();
-			}
-		});
 	},
 	
 	getMoreTweets: function (req, res, next) {
 		var category = req.params.category.toLowerCase();
-		if (!nimble.isValidCategory(category)) {
+		if (nimble.isValidCategory(category)) {
+			var tweetProvider = new TweetProvider(category);
+			tweetProvider.getPagedTweets(req.body.page, function(err, docs) {
+				if (!err) {
+					res.json(docs);
+				} else {
+					next();
+				}
+			});
+		} else {
 			next();
 		}
-		
-		var tweetProvider = new TweetProvider(category);
-		tweetProvider.getPagedTweets(req.body.page, function(err, docs) {
-			if (!err) {
-				res.json(docs);
-			} else {
-				next();
-			}
-		});
 	}
 };
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -7,56 +7,56 @@ var FriendProvider = require('../providers/friend_provider').FriendProvider;
 module.exports = {
 	displayUserTweets: function (req, res, next) {
 		var category = req.params.category.toLowerCase();
-		if (!nimble.isValidCategory(category)) {
+		if (nimble.isValidCategory(category)) {
+			var tweetProvider = new TweetProvider(category);
+			var friendProvider = new FriendProvider(category);
+		
+			tweetProvider.getPagedTweetsByUser(1, req.params.user_name, function(err, docs) {
+				if (!err && docs.length > 0) {
+					friendProvider.find(function(err, friends) {
+						if (!err) {
+							var viewModel = {
+								user_name: req.params.user_name,
+								tweets: docs,
+								friends: friends,
+								category: category
+							};
+				
+							var userInfo = {};
+							userInfo.user_name = docs[0].user_name;
+							userInfo.full_name = docs[0].full_name;
+				
+							res.render('user/user', {
+								title: userInfo.full_name + ' (@' + userInfo.user_name + ')',
+								model: viewModel
+							});
+						} else {
+							next();
+						}
+					});
+				} else {
+					next();
+				}
+			});
+		} else {
 			next();
 		}
-		
-		var tweetProvider = new TweetProvider(category);
-		var friendProvider = new FriendProvider(category);
-		
-		tweetProvider.getPagedTweetsByUser(1, req.params.user_name, function(err, docs) {
-			if (!err && docs.length > 0) {
-				friendProvider.find(function(err, friends) {
-					if (!err) {
-						var viewModel = {
-							user_name: req.params.user_name,
-							tweets: docs,
-							friends: friends,
-							category: category
-						};
-				
-						var userInfo = {};
-						userInfo.user_name = docs[0].user_name;
-						userInfo.full_name = docs[0].full_name;
-				
-						res.render('user/user', {
-							title: userInfo.full_name + ' (@' + userInfo.user_name + ')',
-							model: viewModel
-						});
-					} else {
-						next();
-					}
-				});
-			} else {
-				next();
-			}
-		});
 	},
 	
 	getMoreTweetsByUser: function (req, res, next) {
 		var category = req.params.category.toLowerCase();
-		if (!nimble.isValidCategory(category)) {
+		if (nimble.isValidCategory(category)) {	
+			var tweetProvider = new TweetProvider(category);
+		
+			tweetProvider.getPagedTweetsByUser(req.body.page, req.body.user_name, function(err, docs) {
+				if (!err) {
+					res.json(docs);
+				} else {
+					next();
+				}
+			});
+		} else {
 			next();
 		}
-		
-		var tweetProvider = new TweetProvider(category);
-		
-		tweetProvider.getPagedTweetsByUser(req.body.page, req.body.user_name, function(err, docs) {
-			if (!err) {
-				res.json(docs);
-			} else {
-				next();
-			}
-		});
 	}
 };
