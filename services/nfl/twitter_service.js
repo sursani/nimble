411092ebadc8d9a4997198ad76ca00cfa643248c
Shane Ursani <shaneursani@gmail.com>
2012-02-22 08:03:56 -0500

    Update services/nfl/twitter_service.js

--- services/nfl/twitter_service.js
+++ services/nfl/twitter_service.js
@@ -4,8 +4,8 @@ var twitter = require('twitter');
 var TweetProvider = require('../../providers/tweet_provider').TweetProvider;
 
 var tweetProvider = new TweetProvider();
-var nflAccessToken = "405881726-PwITAhRURYAB27XimgzTUg5tJmjaGZSscCoRXxSs";
-var nflAccessTokenSecret= "OA8IuLk5fXRwbxoKgmeT9EJvIiGBKk1d14agSFry1Iw";
+var nflAccessToken = "";
+var nflAccessTokenSecret= "";
 
 var nflDaemonProcess = function() {
 	var category = 'nfl';
