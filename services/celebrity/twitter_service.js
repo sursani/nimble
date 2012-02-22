a28acfb61c460ff0bd7e9e94d6c2959747ea5e1e
Shane Ursani <shaneursani@gmail.com>
2012-02-22 08:01:54 -0500

    Update services/celebrity/twitter_service.js

--- services/celebrity/twitter_service.js
+++ services/celebrity/twitter_service.js
@@ -4,8 +4,8 @@ var twitter = require('twitter');
 var TweetProvider = require('../../providers/tweet_provider').TweetProvider;
 
 var tweetProvider = new TweetProvider();
-var celebAccessToken = "386324335-xUNH0SVvmaz87RQ246VPUaNTls2wftrYPH4T425Y";
-var celebAccessTokenSecret= "kuA0Ub5GXnVT8cOWe8Rn9OcUHhSBaHqVOZ2fbDg0CHo";
+var celebAccessToken = "";
+var celebAccessTokenSecret= "";
 
 var celebDaemonProcess = function() {
 	var category = 'celebrity';
