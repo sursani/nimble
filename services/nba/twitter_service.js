8b56c282b82f58b2883a96c8b5fa3e35cd83ae41
Shane Ursani <shaneursani@gmail.com>
2012-02-22 08:02:47 -0500

    Update services/nba/twitter_service.js

--- services/nba/twitter_service.js
+++ services/nba/twitter_service.js
@@ -4,8 +4,8 @@ var twitter = require('twitter');
 var TweetProvider = require('../../providers/tweet_provider').TweetProvider;
 
 var tweetProvider = new TweetProvider();
-var nbaAccessToken = "399110344-H3TlQlfTKQwBZo7yfilrrTUIRZ0turcg3BQJTsjg";
-var nbaAccessTokenSecret= "KFi3M2DVQxfYVU0Mk7HXKNtdjm6v6QClpwtftW6rY";
+var nbaAccessToken = "";
+var nbaAccessTokenSecret= "";
 
 var nbaDaemonProcess = function() {
 	var category = 'nba';
